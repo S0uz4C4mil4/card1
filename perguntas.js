0d2e859737f3732b67c9506ecc850d3ea984bba6
Camila <00001100990732sp@al.educacao.sp.gov.br>
2024-11-24 11:32:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    ' Cinema ',
+    'Quem é o Ator que faz o Homem-Aranha em sua nova Trilogia?',
+    'Tom Holland'
+)
+
+criaCartao(
+    'Historia',
+    'Qual o ano de inicio da guerra fria?',
+    '12 de março de 1947'
+)
+
+criaCartao(
+    'Cultura PoP',
+    'Personagem da turma da monica que troca o R pelo L?',
+    'Cebolinha'
+)
+
+criaCartao(
+    'Fato',
+    'Quanto vou ficar de media?',
+    'Um 10 Com certeza!'
+)
